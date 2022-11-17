07d1e58fa4a0c02890909f13e041d21c2bea4505
mantinhas <hugofbmantinhas@gmail.com>
2022-11-17 17:41:07 +0100

    almost

--- contracts/contract.sol
+++ contracts/contract.sol
@@ -7,10 +7,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract CarLeaseSystem {
     struct Lease {
         State state;
+        uint256 start_time;
         uint256 end_time;
         address payable buyer;
         uint256 quote;
-        uint256 payments_out_of_12;
+        uint256 payments;
     }
     
     enum State { Available, Locked, Leased }
@@ -23,7 +24,7 @@ contract CarLeaseSystem {
     constructor( string[] memory _carURIs ) public {
         seller = payable(msg.sender);
         for (uint256 i=0; i<_carURIs.length; i++){
-            cars[car.mintCAR(_carURIs[i])] = Lease(State.Available, 0, payable(msg.sender)); // Only first argument matters
+            cars[car.mintCAR(_carURIs[i])] = Lease(State.Available, 0,0, payable(msg.sender),0,0); // Only first argument matters
         }
     }
 
@@ -43,15 +44,17 @@ contract CarLeaseSystem {
     event SignedBySeller();
     event NotSignedBySeller();
 
-    function confirmPurchase(uint256 carTokenID, uint256 current_car_mileage, uint256 driver_experience, uint256 mileage_cap, uint256 end_time
+    function confirmPurchase(uint256 carTokenID, uint256 current_car_mileage, uint256 driver_experience, uint256 mileage_cap, uint256 start_time, uint256 end_time
         /* car token, duration, mileage, driver's experience */) public payable inState(carTokenID, State.Available){
-        uint256 price = car.calculatePrice(current_car_mileage, driver_experience, mileage_cap, end_time);
+        uint256 price = car.calculatePrice(current_car_mileage, driver_experience, mileage_cap, end_time-start_time);
         require(msg.value==price*4, "Incorrect value sent.");
 
         emit PurchaseConfirmed();
         cars[carTokenID].buyer = payable(msg.sender);
         cars[carTokenID].quote = price;
-        cars[carTokenID].payments_out_of_12 = 1;
+        cars[carTokenID].payments = 1;
+        cars[carTokenID].start_time = start_time;
+        cars[carTokenID].end_time = end_time;
         value = msg.value; // Locking value in contract
         cars[carTokenID].state=State.Locked;
     }
@@ -71,6 +74,24 @@ contract CarLeaseSystem {
         
         cars[carTokenID].state = State.Available;
         cars[carTokenID].buyer.transfer(value);
+    }
+
+    function deleteLease(uint256 carTokenID) private onlySeller {
+        cars[carTokenID].state = State.Available;
+        cars[carTokenID].buyer = seller;
+    }
+
+    function insolventCustomer(uint256 carTokenID) public onlySeller inState(carTokenID, State.Leased) {
+        uint256 seconds_in_a_month = 2629800;
+        require ( (block.timestamp-cars[carTokenID].start_time) / seconds_in_a_month > cars[carTokenID].payments , "The customer does not currently owe anything." );
+        deleteLease(carTokenID);
+        car.transferOwnership(carTokenID, cars[carTokenID]);
+    }
+
+    function payMonthlyQuote(uint256 carTokenID) payable public inState(carTokenID, State.Leased) { // I dont see any reason to restrict this function to the buyer
+        require(msg.value==cars[carTokenID].quote, "Incorrect value sent.");
+        seller.transfer(msg.value);
+        cars[carTokenID].payments = cars[carTokenID].payments + 1;
     }
 
 }
@@ -101,10 +122,9 @@ contract Car is ERC721URIStorage{
         return tokenID;
     }
 
-    function transferOwnership(uint256 tokenID, CarLeaseSystem.Lease memory lease) public onlyMinter {
-        require(block.timestamp >= lease.end_time, "This car is still under lease");
-
+    function transferOwnership(uint256 tokenID, CarLeaseSystem.Lease memory l) public onlyMinter{
+        require (l.state == CarLeaseSystem.State.Available, "This car is not available for transfer.");
         address payable owner = payable(ownerOf(tokenID));
-        _transfer(owner, lease.buyer, tokenID);
+        _transfer(owner, l.buyer, tokenID);
     }
 }
